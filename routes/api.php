222db153d1f50be81137c32f79886989a4b48cf8
JimyCalvo <jbcm1152@gmail.com>
2023-02-07 13:38:01 -0500

    Sprint-1

--- routes/api.php
+++ routes/api.php
@@ -2,18 +2,28 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\LogoutController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::view('/','pages.home') ->name('home');
+
+//----------------------------------------------------------------
+  // stars-restaurants.com/register=> Registrar
+  Route::get('/register',[RegisterController::class,'show']);
+
+  // stars-restaurants.com/register=> Registrar -POST
+Route::post('/register', [RegisterController::class,'register']);
+
+//----------------------------------------------------------------
+// stars-restaurants.com/login => Logear
+
+Route::get('/login',[LoginController::class,'show'])->name('login');
+
+// stars-restaurants.com/login=> login -POST
+Route::post('/login', [LoginController::class,'login']);
+
+//--------------------------Logout---------------------------------
+
+Route::get('/logout', [LogoutController::class,'logout'])->name('logout');
